2e47d79c4047ae93db54df5d62faab3594466220
David Viña <soivigol1012@gmail.com>
2023-04-19 16:55:46 +0000

    In build and new icon

--- build/soivigol-slider-item/index.asset.php
+++ build/soivigol-slider-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '6eaec13fd7ce02c044c9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'ba64c12f6d27cc20ec51');
--- build/soivigol-slider/index.asset.php
+++ build/soivigol-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '520e83382c037e029c6c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6baebb91b1a0178a2ab2');
